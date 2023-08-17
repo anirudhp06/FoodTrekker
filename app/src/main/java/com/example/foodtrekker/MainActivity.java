5c84de7c0681a8de353a63d7ddc72cc5752b09c0
Anirudh Parvatikar <71212295+anirudhp06@users.noreply.github.com>
2023-08-17 23:37:17 +0530

    Added Dropdown Menu

--- app/src/main/java/com/example/foodtrekker/MainActivity.java
+++ app/src/main/java/com/example/foodtrekker/MainActivity.java
@@ -1,14 +1,19 @@
 package com.example.foodtrekker;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.widget.*;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    Spinner drop;
+    String[] items={"Hubli","Bangalore","Hasan","Mangalore"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        drop=(Spinner) findViewById(R.id.spinner_dropdown);
+        ArrayAdapter<String>adapter=new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_dropdown_item,items);
+        drop.setAdapter(adapter);
     }
 }
