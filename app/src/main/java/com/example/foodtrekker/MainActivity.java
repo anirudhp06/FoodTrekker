88307f8417c4792bf84e1bebd235d6a862be0859
Anirudh Parvatikar <71212295+anirudhp06@users.noreply.github.com>
2023-08-18 23:33:58 +0530

    Added Listener for dropdown menu

--- app/src/main/java/com/example/foodtrekker/MainActivity.java
+++ app/src/main/java/com/example/foodtrekker/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.foodtrekker;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.util.Log;
+import android.view.View;
 import android.widget.*;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
     Spinner drop;
+    TextView info;
     String[] items={"Hubli","Bangalore","Hasan","Mangalore"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +19,19 @@ public class MainActivity extends AppCompatActivity {
         ArrayAdapter<String>adapter=new ArrayAdapter<>(this,
                 android.R.layout.simple_spinner_dropdown_item,items);
         drop.setAdapter(adapter);
+        info=(TextView)findViewById(R.id.info);
+        drop.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                String selectedItem=items[i];
+                Log.d("Dropdown","Selected item:"+selectedItem);
+                info.setText("You have selected:"+selectedItem+" city");
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                Log.d("No DropDown","No item Selected");
+            }
+        });
     }
 }
